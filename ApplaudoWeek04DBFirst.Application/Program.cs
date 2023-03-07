2627369635a64c0e59c0bd198b5c7b879b0e96bc
Luis <luis.w.rivera.c@gmail.com>
2023-03-07 14:03:32 -0500

    add application project, data and domain class library

---
+++ ApplaudoWeek04DBFirst.Application/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ApplaudoWeek04DBFirst.Application
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
